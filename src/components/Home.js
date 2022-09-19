d3cc3b1daaf983e5bc6e9fded98b58796433dc86
Elpis-alpha <elpis409@gmail.com>
2022-09-19 13:25:33 +0100

    stuffs

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+
+const Home = () => {
+
+	return (
+
+		<HomeStyle>
+
+		</HomeStyle>
+
+	)
+
+}
+
+const HomeStyle = styled.div`
+
+	width: 100%;
+`
+
+export default Home
